2dfc9512d698106a7323ad372ab3ac5512d1c406
dxj1113 <xijun.du@gmail.com>
2017-05-11 13:51:28 +0800

    2017-05-11 13:51update

---
+++ ms-vscode.powershell-1.0.0/out/languageServerClient.js
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+"use strict";
+var vscode_languageclient_1 = require("vscode-languageclient");
+exports.RequestType0 = vscode_languageclient_1.RequestType0;
+exports.RequestType = vscode_languageclient_1.RequestType;
+exports.NotificationType0 = vscode_languageclient_1.NotificationType0;
+exports.NotificationType = vscode_languageclient_1.NotificationType;
+//# sourceMappingURL=languageServerClient.js.map
