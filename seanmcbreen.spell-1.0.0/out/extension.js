54e6199d0dd382cd9a33685a6e006930c7d1a3e7
dxj1113 <xijun.du@gmail.com>
2017-06-20 18:32:11 +0800

    2017-06-20 18:32update

--- seanmcbreen.spell-1.0.0/out/extension.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const spellProvider_1 = require("./features/spellProvider");
-function activate(context) {
-    let linter = new spellProvider_1.default();
-    linter.activate(context);
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
