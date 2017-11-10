0a212763a03e08ef6a574978c4d5587a8fe63a3f
dxj1113 <xijun.du@gmail.com>
2017-11-10 18:29:46 +0800

    2017-11-10 18:30update

--- streetsidesoftware.code-spell-checker-1.4.8/server/src/codeActions.d.ts
+++
@@ -1,3 +0,0 @@
-import { TextDocuments, Command, CodeActionParams } from 'vscode-languageserver';
-import { CSpellUserSettings } from 'cspell';
-export declare function onCodeActionHandler(documents: TextDocuments, fnSettings: () => CSpellUserSettings): (params: CodeActionParams) => Promise<Command[]>;
